8f6c734f84a044224b3a3904c36d4ac5b3ecc921
Emma Wahnish <ewahnish@gmail.com>
2022-12-03 10:45:46 -0300

    auxiliar two

--- src/controller/uploadController.js
+++ src/controller/uploadController.js
@@ -1,4 +1,4 @@
-import usersService from '../Models/Users.js';
+import auxiliarService from '../Models/Auxiliar.js';
 
 export const testRoute = async (req, res,) => {
   res.json({ message: `Soy una ruta de prueba ;)` })
@@ -7,16 +7,20 @@ export const testRoute = async (req, res,) => {
 export const uploadRoute = async (req, res, next) => {
 
   let url = '/images/' + req.file.filename
-  let avatar = {
-    avatar: url
+  let auxurl = {
+    auxurl: url
   }
-
-  console.log("here is the avatar >>> ", avatar)
   let file = req.file
   if (!file) {
     const error = new Error('Error subiendo archivo')
     error.httpStatusCode = 400
     return next(error)
   }
-  
+  try {
+    await auxiliarService.findOneAndUpdate({ _id: 1 }, auxurl, { returnOriginal: false })
+  }
+  catch (error) {
+    console.log(error)
+  }
+
 }
