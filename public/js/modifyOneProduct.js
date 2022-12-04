d3f38d0c4d0c2982474fc4f5049e2100a8135bd0
Emma Wahnish <ewahnish@gmail.com>
2022-12-04 12:44:57 -0300

    prettier

--- public/js/modifyOneProduct.js
+++ public/js/modifyOneProduct.js
@@ -5,30 +5,30 @@ import { LocalStorageService } from './localStorageService.js';
 let headers_object = build_header();
 
 const modifyOneProduct = (modifiedProduct) => {
-    
+
     let newProduct = LocalStorageService.getItem("newProduct");
 
     const productRoute = `/api/productos/${modifiedProduct.id}`
 
     const requestOptions = {
-        method:'PUT',
+        method: 'PUT',
         headers: headers_object,
         body: JSON.stringify(modifiedProduct),
     };
 
     fetch(productRoute, requestOptions)
-    .then(async res => {
-        const data = await res.json();
-        if ((newProduct != null) && (!newProduct.isNew)) {
-        alert('Modificación exitosa');
-        let productId = modifiedProduct.id;
-        showOneProduct(productId);
-        }
-    })
-    .catch(error => {
-        console.log('Se produjo el siguiente error: ', error);
-    })
-    
+        .then(async res => {
+            const data = await res.json();
+            if ((newProduct != null) && (!newProduct.isNew)) {
+                alert('Modificación exitosa');
+                let productId = modifiedProduct.id;
+                showOneProduct(productId);
+            }
+        })
+        .catch(error => {
+            console.log('Se produjo el siguiente error: ', error);
+        })
+
 }
 
 export default modifyOneProduct;
